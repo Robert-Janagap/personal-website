05bf1953a540ad55f4ce71f60aaa2a41604fdc07
Robert Janagap <janagaprobert@gmail.com>
2015-02-09 08:13:25 +0800

    Setting up the project.

---
+++ config.rb
@@ -0,0 +1,11 @@
+project_type     = :stand_alone
+http_path        = "/"
+sass_dir         = "src/scss"
+css_dir          = "build/assets/css"
+images_dir       = "images"
+fonts_dir        = "fonts"
+javascript_dir   = "js"
+line_comments    = false
+preferred_syntax = :scss
+output_style     = :expanded
+relative_assets  = true
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+/*
+gulp plugins
+ */
+var gulp    = require('gulp');
+/** local server */
+var connect = require('gulp-connect');
+/** compiler */
+var jade    = require('gulp-jade'),
+	sass    = require('gulp-sass');
+/** Framework */
+var	compass = require('gulp-compass');
+/** utilities */
+var changed = require('gulp-changed'),
+	autoprefix = require('gulp-autoprefixer'),
+	watch = require('gulp-watch');
+/*
+gulp task runner
+ */
+
+/** local server */
+gulp.task('server', function(){
+	connect.server({
+		livereload: true
+	})
+});
+
+/*
+Compiler
+ */
+/** jade */
+gulp.task('jade', function(){
+	var src = './src/jade/**/*.jade';
+	gulp.src(src)
+		.pipe(changed(src))
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./build/'));
+});
+/** sass */ //use compass for mixin and grid
+gulp.task('sass', function(){
+	var src = './src/scss/main.scss';
+	gulp.src(src)
+		.pipe(changed(src))
+		.pipe(sass())
+		.pipe(autoprefix())
+		.pipe(gulp.dest('./build/assets/css/'));
+});
+//////
+/*
+Framework
+ */
+gulp.task('compass',function(){
+	var src='./src/scss/*.scss';
+	gulp.src(src)
+	  .pipe(changed(src))
+	  .pipe(compass({
+	  	config_file: './config.rb',
+	  	css: 'build/assets/css',
+	  	sass: 'src/scss',
+	  	require: ['susy', 'breakpoint']
+	  }))
+	  .pipe(gulp.dest('./build/assets/css/'))
+});
+/////
+/** watch task*/
+gulp.task('watch', function(){
+	gulp.watch('src/jade/*.jade',['jade'])
+	gulp.watch('src/scss/**/*.scss',['compass'])
+});
+
+gulp.task('livereload', function(){
+	watch(['./build/index.html', './build/assets/css/main.css'])
+		.pipe(connect.reload());
+});
+/** gulp default run */
+gulp.task('default',['server', 'watch', 'livereload']);
