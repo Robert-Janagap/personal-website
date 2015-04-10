e2713d84e126699792dd7b5d6d346d28e72879c1
Robert Janagap <janagaprobert@gmail.com>
2015-04-10 19:57:05 +0800

    add animation.

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,51 @@
 (function($){
-	$('.nav_icon').on('click', function(){
-		$('.header_responsive').slideToggle();
-	});
+	var win = $(window),
+		scrollTop_btn = $('.scroll_top--btn'),
+		headerNav_responsive = $('.header_responsive'),
+		headerNav_icon = $('.nav_icon'),
+		overlay = $('.overlay');
+
 	$(window).on('scroll', function(){
-		if($(window).scrollTop() > 500){
-			$('.scroll_top--btn').fadeIn(600);
-			console.log($(window).scrollTop());
+
+		(win.scrollTop() > 500) ? scrollTop_btn.fadeIn(600) : scrollTop_btn.fadeOut(600);
+
+		if( win.scrollTop() > 80 ){
+			$('.header--fixed').show(400);
+			headerNav_icon.addClass('nav_icon--fixed');
+			headerNav_responsive.css('top', '60px');
 		}else{
-			$('.scroll_top--btn').fadeOut(600);
-			console.log($(window).scrollTop());
+			if(win.width() >= 960){
+				headerNav_responsive.slideUp(400);
+				overlay.fadeOut(400);
+				headerNav_icon.removeClass('nav_icon--click');
+			}
+			$('.header--fixed').hide(400);
+			headerNav_icon.removeClass('nav_icon--fixed');
+			headerNav_responsive.css('top', '80px');
 		}
 	});
+
+	headerNav_icon.on('click', function(e){
+		e.preventDefault();
+		headerNav_responsive.slideToggle();
+		$(this).toggleClass('nav_icon--click');
+		overlay.fadeToggle(600);
+	});
+
+	overlay.on('click', function(e){
+		e.preventDefault();
+		$(this).fadeOut(600);
+		$('.header--fixed').hide(400);
+		headerNav_responsive.slideUp(400);
+		headerNav_icon.removeClass('nav_icon--fixed');
+	});
+
+
 	$('.scroll_top--btn').on('click',function(){
 		$("html, body").animate({
 			scrollTop: 0
 		}, 600);
 	});
+
+
 })(jQuery);
